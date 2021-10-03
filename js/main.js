bb3cfcb943aae51f4978044738c2b219c530fb5d
katenichols <kat-nik79@ya.ru>
2021-10-03 17:36:43 +0500

    Реализована генерация массива из 25 элементов-объектов

--- js/main.js
+++ js/main.js
@@ -1,8 +1,41 @@
-const randomIntNumber = (min, max) => {
+// Список фраз для комментариев
+const COMMENT_PHRASES = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
+];
+
+// Список имен комментаторов
+const AUTHORS_COMMENTS = [
+  'Александр',
+  'Альбина',
+  'Антон',
+  'Артём',
+  'Владимир',
+  'Георгий',
+  'Гузель',
+  'Дарья',
+  'Евгения',
+  'Елена',
+  'Ирина',
+  'Любовь',
+  'Максим',
+  'Михаил',
+  'Ольга',
+  'Роман',
+  'Сергей',
+  'Софья',
+];
+
+// Функция получения случайного числа из диапазона
+const getRandomIntNumber = (min, max) => {
   min = Math.ceil(min);
   max = Math.floor(max);
 
-  if (max === min) {
+  if (min < 0 || max < 0) {
     return -1;
   } else if (max < min) {
     [min, max] = [max, min];
@@ -11,8 +44,48 @@ const randomIntNumber = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1)) + min; //источник - MDN
 };
 
-randomIntNumber(4,2);
+// Функция получения элемента массива
+const getRandomArrayElement = (elements) => elements[getRandomIntNumber(0, elements.length - 1)];
 
-const commentLengthCheck = (commentString, commentMaxLength) => commentString.length <= commentMaxLength;
+// Функция составления объекта комментария
+let randomComments = 0;
+let idCommentsNumber = 0;
 
-commentLengthCheck('Проверка текста на длину строки', 20);
+const createCommentsItem = () => {
+  let messageText = '';
+
+  if (getRandomIntNumber(1, 2) === 2) {
+    messageText = `${getRandomArrayElement(COMMENT_PHRASES)} ${  getRandomArrayElement(COMMENT_PHRASES)}`;
+  } else {
+    messageText = getRandomArrayElement(COMMENT_PHRASES);
+  }
+
+  idCommentsNumber++;
+
+  return {
+    id: idCommentsNumber,
+    avatar: `img/avatar${String(getRandomIntNumber(1, 6))}.svg`,
+    message: messageText,
+    name: getRandomArrayElement(AUTHORS_COMMENTS),
+  };
+};
+
+// Функция создания общего массива
+let idDescNumber = 0;
+
+const createDescPictureItem = () => {
+  idDescNumber++;
+  randomComments = getRandomIntNumber(1, 4);
+  idCommentsNumber = 0;
+  return {
+    id: idDescNumber,
+    url: `photos/${String(idDescNumber)}.jpg`,
+    description: `Happy life! Часть ${String(idDescNumber)}`,
+    likes: getRandomIntNumber(15, 200),
+    comments: Array.from({length: randomComments}, createCommentsItem),
+  };
+};
+
+const descPicture = Array.from({length: 25}, createDescPictureItem);
+
+console.log(descPicture);

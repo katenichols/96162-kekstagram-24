0ada0218c76de1ed9dc06e62cee37a2592780b58
katenichols <kat-nik79@ya.ru>
2021-11-02 22:49:04 +0500

    Доделать показ комментариев частями

--- js/big-picture.js
+++ js/big-picture.js
@@ -14,8 +14,8 @@ const socialCommentsCount = bigPictureSocial.querySelector('.social__comment-cou
 const commentsLoader = bigPictureSocial.querySelector('.comments-loader');
 const socialCaption = bigPictureSocial.querySelector('.social__caption');
 let showArray = [];
-const numberOfShowSecondIteration = 2;
-let numberOfShow = 1;
+let commentsToShow = 0;
+let count = LIMIT_OF_SHOW_COMMENTS;
 let shownComments = 0;
 
 
@@ -58,80 +58,41 @@ const commentsCountLabel = (shownCommentsNumbers, totalLength) => {
   }
 };
 
+const showNextComments = (lastLength) => {
+  if (LIMIT_OF_SHOW_COMMENTS >= lastLength) {
+    commentsLoader.removeEventListener('click', showNextComments());
+    commentsLoader.classList.add('hidden');
+    return;
+  }
+
+  showArray[count++].classList.remove('hidden');
+  if (count < showArray.length) {
+    showArray[count].classList.remove('hidden');
+  }
+
+  // вызываем функцию показа информации о комментах
+  commentsCountLabel(shownComments, length);
+};
+
 // Функция показа комментариев по 5
 const showComments = (commentsArray) => {
-  const length = commentsArray.length;
-  const hideArray = Array.from(socialComments.querySelectorAll('.social__comment'));
-  showArray = [];
+  showArray = Array.from(socialComments.querySelectorAll('.social__comment'));
 
   // прячем лоадер, если комментов меньше лимита
-  if (hideArray.length <= LIMIT_OF_SHOW_COMMENTS) {
+  if (showArray.length <= LIMIT_OF_SHOW_COMMENTS) {
     commentsLoader.classList.add('hidden');
+    return;
   } else {
     commentsLoader.classList.remove('hidden');
-  }
-
-  // прячем все комментарии
-  for (let i = 0; i < length; i++) {
-    hideArray[i].classList.add('hidden');
-  }
-
-  // добавляем комментарии в массив для показа
-  if (hideArray.length < LIMIT_OF_SHOW_COMMENTS) {
-    for (let y = 0; y < hideArray.length; y++) {
-      showArray.push(hideArray[y]);
-      showArray[y].classList.remove('hidden');
-    }
-  } else {
-    for (let y = 0; y < LIMIT_OF_SHOW_COMMENTS; y++) {
-      showArray.push(hideArray[y]);
-      showArray[y].classList.remove('hidden');
+    for (let i = LIMIT_OF_SHOW_COMMENTS; i < showArray.length; i++) {
+      console.log(i);
+      showArray[i].classList.add('hidden');
+      commentsToShow++;
     }
   }
 
   // обрабатываем клик по лоадеру
-  commentsLoader.addEventListener('click', () => {
-    numberOfShow++;
-    // удаляем из массива комментов уже показанные комменты
-    hideArray.splice(0, LIMIT_OF_SHOW_COMMENTS);
-    // изменяем число показанных комментов
-    const length = commentsArray.length;
-    shownComments = LIMIT_OF_SHOW_COMMENTS * numberOfShow;
-
-    // выравниваем число показанных комментов по их общему количеству
-    if (shownComments > length) {
-      shownComments = length;
-    }
-
-    // в массиве к показу скрываем уже показанные комменты, а затем удаляем их
-    for (let k = 0; k < LIMIT_OF_SHOW_COMMENTS; k++) {
-      showArray[k].classList.add('hidden');
-    }
-    showArray.splice(0, LIMIT_OF_SHOW_COMMENTS);
-
-    // добавляем в массив к показу новую порцию комментов
-    if (hideArray.length <= LIMIT_OF_SHOW_COMMENTS) {
-      commentsLoader.classList.add('hidden');
-      numberOfShow = numberOfShowSecondIteration;
-
-      for (let l = 0; l < hideArray.length; l++) {
-        showArray.push(hideArray[l]);
-        // if (showArray.length > 0 && showArray[l].classList.contains('hidden')) {
-          showArray[l].classList.remove('hidden');
-        // }
-      }
-    } else {
-      for (let l = 0; l < LIMIT_OF_SHOW_COMMENTS; l++) {
-        showArray.push(hideArray[l]);
-        // if (showArray.length > 0 && showArray[l].classList.contains('hidden')) {
-          showArray[l].classList.remove('hidden');
-        // }
-      }
-    }
-
-    // вызываем функцию показа информации о комментах
-    commentsCountLabel(shownComments, length);
-  });
+  commentsLoader.addEventListener('click', showNextComments(commentsToShow));
 };
 
 const drowBigPicture = (src, likes, comments, description, commentsArray) => {
@@ -142,7 +103,8 @@ const drowBigPicture = (src, likes, comments, description, commentsArray) => {
   socialCaption.textContent = description;
   shownComments = LIMIT_OF_SHOW_COMMENTS;
 
-  commentsCountLabel(shownComments, Number.parseInt(comments.textContent));
+  // commentsCountLabel(shownComments, Number.parseInt(comments.textConten, 10));
+  commentsCountLabel(shownComments, commentsArray.length);
   copyElement(commentsArray);
   showComments(commentsArray);
 };
